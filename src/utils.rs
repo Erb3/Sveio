9b08d91ed1d46e337657259aa139e140b14f6715
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-06-02 10:09:54 +0200

    tests: test score calculation

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,22 @@ pub fn calculate_score(kms: f64) -> u64 {
 
 	(1500 - kilometers).div_ceil(4)
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn can_calculate_score() {
+		assert_eq!(calculate_score(1500.0), 0);
+		assert_eq!(calculate_score(1501.0), 0);
+		assert_eq!(calculate_score(1504.0), 0);
+		assert_eq!(calculate_score(12893.12), 0);
+		assert_eq!(calculate_score(0.0), 375);
+		assert_eq!(calculate_score(0.2), 375);
+		assert_eq!(calculate_score(0.99), 375);
+		assert_eq!(calculate_score(1.0), 375);
+		assert_eq!(calculate_score(3.9), 375);
+		assert_eq!(calculate_score(4.0), 374);
+	}
+}
