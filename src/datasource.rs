e161be454ffbcf9b3c53dc8772b77f4d3014ec5c
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-06-18 20:36:19 +0200

    refactor: improved randomness
    Fixes #6
    Fixes #41

--- src/datasource.rs
+++ src/datasource.rs
@@ -1,3 +1,5 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Clone)]
@@ -14,7 +16,34 @@ pub struct AnonymizedCity<'a> {
 	pub name: &'a str,
 }
 
-pub async fn get_cities() -> Vec<City> {
-	serde_json::from_str(include_str!("../cities.json"))
-		.expect("cities.json does not have correct format.")
+pub struct Datasource {
+	pub cities: Vec<City>,
+	length: usize,
+	index: usize,
+}
+
+impl Datasource {
+	pub fn get_next(&mut self) -> &City {
+		let city: &City = self.cities.get(self.index).unwrap();
+
+		self.index += 1;
+		if self.index == self.length - 1 {
+			self.index = 0;
+		}
+
+		return city;
+	}
+}
+
+pub async fn new() -> Datasource {
+	let mut cities: Vec<City> = serde_json::from_str(include_str!("../cities.json"))
+		.expect("cities.json does not have correct format.");
+
+	cities.shuffle(&mut thread_rng());
+
+	Datasource {
+		cities: cities.clone(),
+		length: cities.len(),
+		index: 0,
+	}
 }
