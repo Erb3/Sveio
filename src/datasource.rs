80112e9624768aee27e61eb715bd323e949750ee
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-06-30 12:08:15 +0200

    tests: add tests for datasource

--- src/datasource.rs
+++ src/datasource.rs
@@ -29,11 +29,37 @@ pub struct Datasource {
 	pub cities: Vec<City>,
 }
 
-pub async fn new() -> Datasource {
-	let mut cities: Vec<City> = serde_json::from_str(include_str!("../cities.json"))
-		.expect("cities.json does not have correct format.");
+impl Datasource {
+	pub async fn new() -> Datasource {
+		let mut cities: Vec<City> = serde_json::from_str(include_str!("../cities.json"))
+			.expect("cities.json does not have correct format.");
 
-	cities.shuffle(&mut thread_rng());
+		cities.shuffle(&mut thread_rng());
 
-	Datasource { cities }
+		Datasource { cities }
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[tokio::test]
+	async fn can_load_cities() {
+		assert_ne!(Datasource::new().await.cities.len(), 0);
+	}
+
+	#[tokio::test]
+	async fn cities_are_randomized() {
+		let datasource1 = Datasource::new().await;
+		let datasource2 = Datasource::new().await;
+
+		// Check if both the first and second city in the datasources matches
+		// If it does, this suggests that the cities are not randomized
+		assert!(
+			datasource1.cities.get(0).unwrap().name != datasource2.cities.get(0).unwrap().name
+				&& (datasource1.cities.get(1).unwrap().name
+					!= datasource2.cities.get(1).unwrap().name)
+		);
+	}
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-pub fn calculate_score(kms: f64) -> u64 {
+pub(crate) fn calculate_score(kms: f64) -> u64 {
 	let kilometers = kms.floor() as u64;
 	if kilometers > 1500 {
 		return 0;
